b28b3904c008391ee4814aedb7387111953edb6f
Juan Halleran <juanhalleran@gmail.com>
2016-12-11 11:55:19 -0500

    Add files via upload

---
+++ EncoderTestQuaderatureV_1.ino
@@ -0,0 +1,84 @@
+/*
+ int    val; 
+ int    encoder0PinA     = 2;
+ int    encoder0PinB     = 3;
+ double encoder0Pos      = 0;
+ int    encoder0PinALast = LOW;
+ int    n                = LOW;
+
+ void setup() { 
+   pinMode (encoder0PinA,INPUT);
+   pinMode (encoder0PinB,INPUT);
+   Serial.begin (9600);
+ } 
+
+ void loop() { 
+   n = digitalRead(encoder0PinA);
+   if ((encoder0PinALast == LOW) && (n == HIGH)) {
+     if (digitalRead(encoder0PinB) == LOW) {
+       encoder0Pos--;
+     } else {
+       encoder0Pos++;
+     }
+     Serial.println (encoder0Pos);
+   } 
+   encoder0PinALast = n;
+ }
+  */
+enum PinAssignments {
+  encoderPinA = 2,
+  encoderPinB = 3,
+  clearButton = 8
+};
+
+volatile double encoderPos = 0;
+unsigned int lastReportedPos = 1;
+
+boolean A_set = false;
+boolean B_set = false;
+
+
+void setup() {
+
+  pinMode(encoderPinA, INPUT); 
+  pinMode(encoderPinB, INPUT); 
+  pinMode(clearButton, INPUT);
+  digitalWrite(encoderPinA, HIGH);  // turn on pull-up resistor
+  digitalWrite(encoderPinB, HIGH);  // turn on pull-up resistor
+  digitalWrite(clearButton, HIGH);
+
+// encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+// encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE);
+
+  Serial.begin(9600);
+}
+
+
+void loop(){ 
+  if (lastReportedPos != encoderPos) {
+    Serial.print((encoderPos/4)*(.036) , DEC);
+    Serial.println();
+    lastReportedPos = encoderPos;
+  }
+  if (digitalRead(clearButton) == LOW)  {
+    encoderPos = 0;
+  }
+}
+
+// Interrupt on A changing state
+void doEncoderA(){
+  // Test transition
+  A_set = digitalRead(encoderPinA) == HIGH;
+  // and adjust counter + if A leads B
+  encoderPos += (A_set != B_set) ? +1 : -1;
+}
+
+// Interrupt on B changing state
+void doEncoderB(){
+  // Test transition
+  B_set = digitalRead(encoderPinB) == HIGH;
+  // and adjust counter + if B follows A
+  encoderPos += (A_set == B_set) ? +1 : -1;
+}
